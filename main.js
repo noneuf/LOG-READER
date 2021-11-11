d1222d5b74bf67e8e0740643557f3d64891ac0f4
Nathan <goelnathan@gamil.com>
2021-11-11 22:50:23 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const delimiters = {
+  lineDelimiter:
+    '___________________________________________________________________________________________________',
+  elementDelimiter: '|',
+  imgPathDelimiter: ' נתיב לצילום מסך: ',
+};
+
+const input = document.querySelector('input[type="file"]');
+
+input.addEventListener(
+  'change',
+  (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      reader.result.split(delimiters.lineDelimiter).map((line) => {
+        const splitedLine = line.split(delimiters.elementDelimiter);
+        splitedLine.forEach((element) => {
+          if (element.includes(delimiters.imgPathDelimiter)) {
+            const imgPath = element.split(delimiters.imgPathDelimiter);
+            console.log(imgPath[1]);
+            const node = document.createElement('img');
+            node.setAttribute('src', imgPath[1]);
+            node.setAttribute('height', '150');
+            const textnode = document.createTextNode(element);
+            node.appendChild(textnode);
+            document.getElementById('myList').appendChild(node);
+          }
+
+          const node = document.createElement('div');
+          node.className = 'element';
+          const textnode = document.createTextNode(element);
+          node.appendChild(textnode);
+          document.getElementById('myList').appendChild(node);
+        });
+      });
+    };
+    reader.readAsText(input.files[0]);
+  },
+  false
+);
